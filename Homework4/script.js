6056c8e8a3e7e83b6e1c76912b51ced915f7d707
Ritik-Kumar01 <118629689+Ritik-Kumar01@users.noreply.github.com>
2024-07-11 09:11:30 +0530

    Add files via upload

---
+++ Homework4/script.js
@@ -0,0 +1,34 @@
+const searchInput = document.getElementById('search-inp');
+const searchBtn = document.getElementById('search-btn');
+const recipeContainer = document.getElementById('recipe-cont');
+
+
+fetch('https://dummyjson.com/recipes')
+    .then(response => response.json())
+    .then(data => renderRecipes(data.recipes));
+
+searchBtn.addEventListener('click', searchRecipes);
+
+
+function searchRecipes() {
+    const searchTerm = searchInput.value.trim();
+    if (searchTerm) {
+        fetch(`https://dummyjson.com/recipes/search?q=${searchTerm}`)
+            .then(response => response.json())
+            .then(data => renderRecipes(data.recipes));
+    }
+}
+
+function renderRecipes(recipes) {
+    recipeContainer.innerHTML = '';
+    recipes.forEach(recipe => {
+        const recipeCard = document.createElement('div');
+        recipeCard.className = 'recipe-card';
+        recipeCard.innerHTML = `
+            <img src="${recipe.image}" alt="${recipe.name}">
+            <h2>${recipe.name}</h2>
+            <p>Price: ${recipe.price}</p>
+        `;
+        recipeContainer.appendChild(recipeCard);
+    });
+}
